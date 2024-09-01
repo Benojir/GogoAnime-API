2b20bb5c289b02fe55fc41b34b607d0366f9bdc1
Benojir Sultana <124562791+Benojir@users.noreply.github.com>
2024-09-01 17:51:02 +0530

    Create genre.php

---
+++ genre.php
@@ -0,0 +1,29 @@
+<?php
+
+use voku\helper\HtmlDomParser;
+require_once 'composer/vendor/autoload.php';
+
+require_once 'config.php';
+require_once 'functions.php';
+
+if (!isset($_REQUEST['page']) || !isset($_REQUEST['genre'])) {
+    sendErrorResponse("No page number or query.");
+}
+
+$page = $_REQUEST['page'];
+$genre = trim($_REQUEST['genre']);
+
+if(!ctype_digit($page)) {
+    sendErrorResponse("Invalid page number.");
+}
+
+if ($q == "") {
+    sendErrorResponse("Invalid query.");
+}
+
+$gogo_request_url = $gogourl . "/genre/" . $genre . "?page=" . $page;
+
+$anime = extractAnime($gogo_request_url);
+
+header('Content-Type: application/json');
+echo json_encode($anime);
